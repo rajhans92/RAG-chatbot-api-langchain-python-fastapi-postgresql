507d6b3b4b33909fb9d6e9b0ec3d96eac9d22497
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2026-03-18 08:15:05 +0530

    setup project frame

---
+++ app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
+from app.helpers.config import (
+    API_VERSION,
+    API_BASE_NAME
+)
+from app.helpers.exception import (
+    http_exception_handler,
+    validation_exception_handler,
+    value_error_handler,
+    global_exception_handler
+)
+from app.helpers.databaseConnection import engine, Base
+from app.routers import (chatbot)
+
+app  = FastAPI()
+
+# Register exception handlers
+app.add_exception_handler(HTTPException, http_exception_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+app.add_exception_handler(ValueError, value_error_handler)
+app.add_exception_handler(Exception, global_exception_handler)
+
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+app.include_router(chatbot.router, prefix='/{API_BASE_NAME}/{API_VERSION}')
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
